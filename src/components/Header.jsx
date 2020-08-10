6fe808e5477363b19c5a495b83aefc47f089c24a
GiselleDZ <gz@gisellezatonyl.com>
2020-08-10 18:55:17 -0400

    header effect aligned

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,15 +10,17 @@ export default class Header extends Component {
 
             <div className="header__text-box">
                 <div className="header__container">
-                    <h1 className="heading-primary">
-                        <span className="heading-primary--main">
-                            Giselle Zatonyl
-                        </span>
-                        <span className="heading-primary--sub">
-                            Fullstack Software Engineer
-                        </span>
-                    </h1>
-                    <a href="#" className="btn btn--white btn--animated">Nice to meet you!</a>
+                    <div className="heading__wrapper">
+                        <h1 className="heading-primary">
+                            <span className="heading-primary--main">
+                                Giselle Zatonyl
+                            </span>
+                            <span className="heading-primary--sub">
+                                Fullstack Software Engineer
+                            </span>
+                        </h1>
+                        <a href="#" className="btn btn--white btn--animated">Nice to meet you!</a>
+                    </div>
                 </div>
             </div>
         </header>
