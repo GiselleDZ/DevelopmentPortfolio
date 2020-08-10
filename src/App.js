bd0f5e15ea05726772a228ece3556d24d376ce16
GiselleDZ <gz@gisellezatonyl.com>
2020-08-10 17:56:00 -0400

    portfolio home page- header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Footer from './components/footer'
 import Navigation from './components/Navigation'
+import Header from './components/Header'
 import "../public/css/style.css"
 
 
 const App = () => {
   return (
-    <div>
+    <div className="body">
         <Navigation />
+        <Header />
         <Footer />
     </div>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react"
+
+export default class Header extends Component {
+    render(){
+        return(
+            <header className="header">
+                <div className="header__logo-box">
+                    <img className="header__logo" src="#" alt="Logo" />
+                </div>
+
+            <div className="header__text-box">
+                <div className="header__container">
+                    <h1 className="heading-primary">
+                        <span className="heading-primary--main">
+                            Giselle Zatonyl
+                        </span>
+                        <span className="heading-primary--sub">
+                            Fullstack Software Engineer
+                        </span>
+                    </h1>
+                    <a href="#" className="btn btn--white btn--animated">Nice to meet you!</a>
+                </div>
+            </div>
+        </header>
+        )
+    }
+}
