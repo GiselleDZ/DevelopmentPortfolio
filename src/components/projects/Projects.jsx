f7a65039da7f0ebdcd32940e5eddf99de4898666
GiselleDZ <gz@gisellezatonyl.com>
2020-08-26 17:00:42 -0400

    redirected projects thumbs

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -17,7 +17,7 @@ export default class Projects extends Component {
                             <div className="row">
                                 <div className="col-1of5">
                                     <a href="#thirdcultmasks" className="projects__card-thumbs__thumb-wrapper">
-                                        <img src="../../../public/assets/imgs/thirdcult/homepage-thumb.jpg" alt="" className="projects__card-thumbs__thumb-wrapper projects__card-thumbs__thumb-wrapper--thumb-image"/>
+                                        <img src="http://gisellezatonyl.com/images/dev/thirdcult/homepage-thumb.jpg" alt="" className="projects__card-thumbs__thumb-wrapper projects__card-thumbs__thumb-wrapper--thumb-image"/>
                                         <h3 className="heading-tertiary projects__card-thumbs__thumb-wrapper projects__card-thumbs__thumb-wrapper--thumb-title">
                                             Third Cvlt Masks
                                         </h3>
@@ -25,7 +25,7 @@ export default class Projects extends Component {
                                 </div>
                                 <div className="col-1of5">
                                     <a href="#pentimento" className="projects__card-thumbs__thumb-wrapper">
-                                        <img src="../../../public/assets/imgs/pentimento/video-thumb.jpg"  alt="Pentimento Demo Video Thumbnail" className="projects__card-thumbs__thumb-wrapper projects__card-thumbs__thumb-wrapper--thumb-image"/>
+                                        <img src="http://gisellezatonyl.com/images/dev/pentimento/video-thumb.jpg"  alt="Pentimento Demo Video Thumbnail" className="projects__card-thumbs__thumb-wrapper projects__card-thumbs__thumb-wrapper--thumb-image"/>
                                         <h3 className="heading-tertiary  projects__card-thumbs__thumb-wrapper projects__card-thumbs__thumb-wrapper--thumb-title">
                                             Pentimento
                                         </h3>
@@ -33,7 +33,7 @@ export default class Projects extends Component {
                                 </div>
                                 <div className="col-1of5">
                                     <a href="#vchatbot" className="projects__card-thumbs__thumb-wrapper">
-                                        <img src="../../../public/assets/imgs/vchatbot/v-homepage-thumb.jpg" alt="Vi Chat Bot Homepage Thumbnail" className="projects__card-thumbs__thumb-wrapper projects__card-thumbs__thumb-wrapper--thumb-image"/>
+                                        <img src="http://gisellezatonyl.com/images/dev/vchatbot/v-homepage-thumb.jpg" alt="Vi Chat Bot Homepage Thumbnail" className="projects__card-thumbs__thumb-wrapper projects__card-thumbs__thumb-wrapper--thumb-image"/>
                                         <h3 className="heading-tertiary projects__card-thumbs__thumb-wrapper projects__card-thumbs__thumb-wrapper--thumb-title">
                                             Vi Chat Bot
                                         </h3>
@@ -41,7 +41,7 @@ export default class Projects extends Component {
                                 </div>
                                 <div className="col-1of5">
                                     <a href="#coocucachew" className="projects__card-thumbs__thumb-wrapper">
-                                        <img src="../../../public/assets/imgs/coocucachew/main-thumb.jpg"  alt="Coocucachew Home Page Thumbnail" className="projects__card-thumbs__thumb-wrapper projects__card-thumbs__thumb-wrapper--thumb-image"/>
+                                        <img src="http://gisellezatonyl.com/images/dev/coocucachew/main-thumb.jpg"  alt="Coocucachew Home Page Thumbnail" className="projects__card-thumbs__thumb-wrapper projects__card-thumbs__thumb-wrapper--thumb-image"/>
                                         <h3 className="heading-tertiary  projects__card-thumbs__thumb-wrapper projects__card-thumbs__thumb-wrapper--thumb-title">
                                             Coocucachew
                                         </h3>
@@ -49,7 +49,7 @@ export default class Projects extends Component {
                                 </div>
                                 <div className="col-1of5">
                                     <a href="#HiHello" className="projects__card-thumbs__thumb-wrapper">
-                                        <img src="../../../public/assets/imgs/g-dev-site/devsite-homepage-thumb.jpg" alt="Programming Website Homepage Thumbnail" className="projects__card-thumbs__thumb-wrapper projects__card-thumbs__thumb-wrapper--thumb-image"/>
+                                        <img src="http://gisellezatonyl.com/images/dev/g-dev-site/dev-site-main-thumb.jpg" alt="Programming Website Homepage Thumbnail" className="projects__card-thumbs__thumb-wrapper projects__card-thumbs__thumb-wrapper--thumb-image"/>
                                         <h3 className="heading-tertiary  projects__card-thumbs__thumb-wrapper projects__card-thumbs__thumb-wrapper--thumb-title">
                                         Programming Portfolio
                                         </h3>
