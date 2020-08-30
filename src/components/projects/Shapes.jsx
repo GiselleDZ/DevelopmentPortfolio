2a71c6afb811cac6704ad424f2e4b3af5bf5e03b
GiselleDZ <gz@gisellezatonyl.com>
2020-08-30 16:21:16 -0400

    background completed

--- src/components/projects/Shapes.jsx
+++ src/components/projects/Shapes.jsx
@@ -4,8 +4,8 @@ export default class Shapes extends Component{
     render(){
         return(
             <div className="projects__shapes">
-                <div className="projects__background">
-                        <img src="http://gisellezatonyl.com/images/dev/shapes/svg-composition-3-01.svg" alt="composition of abstract shapes"/>
+                <div className="projects__background projects__background--1">
+                    <img src="http://gisellezatonyl.com/images/dev/shapes/svg-composition-3-01.svg" alt="composition of abstract shapes"/>
                 </div>
                 <div className="projects__background projects__background--2">
                     <img src="http://gisellezatonyl.com/images/dev/shapes/shape-4-01.svg" alt="composition of abstract shapes"/>
@@ -25,12 +25,28 @@ export default class Shapes extends Component{
                 <div className="projects__background projects__background--7">
                     <img src="http://www.gisellezatonyl.com/images/dev/shapes/shape-14.svg" alt="composition of abstract shapes"/>
                 </div>
-                {/* <div className="projects__background projects__background--8">
-                    <img src="http://gisellezatonyl.com/images/dev/shapes/shape-7-01.svg" alt="composition of abstract shapes"/>
+                <div className="projects__background projects__background--8">
+                    <img src="http://gisellezatonyl.com/images/dev/shapes/shape-15.svg" alt="composition of abstract shapes"/>
                 </div>
                 <div className="projects__background projects__background--9">
-                    <img src="http://gisellezatonyl.com/images/dev/shapes/shape-7-01.svg" alt="composition of abstract shapes"/>
-                </div> */}
+                    <img src="http://gisellezatonyl.com/images/dev/shapes/shape-16-01.svg" alt="composition of abstract shapes"/>
+                </div>
+                <div className="projects__background projects__background--10">
+                    <img src="http://gisellezatonyl.com/images/dev/shapes/shape-17-01.svg" alt="composition of abstract shapes"/>
+                </div>
+                <div className="projects__background projects__background--11">
+                    <img src="http://gisellezatonyl.com/images/dev/shapes/shape-18-01.svg" alt="composition of abstract shapes"/>
+                </div>
+                <div className="projects__background projects__background--12">
+                    <img src="http://gisellezatonyl.com/images/dev/shapes/shape-19-01.svg" alt="composition of abstract shapes"/>
+                </div>
+                <div className="projects__background projects__background--13">
+                    <img src="http://gisellezatonyl.com/images/dev/shapes/shape-20-01.svg" alt="composition of abstract shapes"/>
+                </div>
+                <div className="projects__background projects__background--14">
+                    <img src="http://gisellezatonyl.com/images/dev/shapes/shape-21-01.svg" alt="composition of abstract shapes"/>
+                </div>
+
             </div>
         )
     }
--- src/components/projects/VChatBot.jsx
+++ src/components/projects/VChatBot.jsx
@@ -41,7 +41,7 @@ export default class VChatBot extends Component {
                 </div>
                 <div className="row">
                     <div className="col-1of3">
-                        <div className="projects__description projects__description--80">
+                        <div className="projects__description projects__description--75">
                             <p className="paragraph paragraph--light paragraph--big">
                                  Vi Chat Bot is an app developed during NYC Coders for Black Lives Matter Hackathon. The app was created to help the Center for Racial Justice in Education disseminate their learning resources, by engaging their community in conversation, and facilitating access with a chat bot built-in to their website, as well as Facebook Messenger.   
                             </p>
