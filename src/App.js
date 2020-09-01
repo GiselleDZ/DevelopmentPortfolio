65fe8ab36ba0133418c38a569a84ea747daf3b85
GiselleDZ <gz@gisellezatonyl.com>
2020-09-01 14:06:59 -0400

    helmet meta tags

--- src/App.js
+++ src/App.js
@@ -4,13 +4,33 @@ import Navigation from './components/Navigation'
 import Header from './components/Header'
 import Intro from './components/Intro'
 import Projects from './components/projects/Projects'
-// import Main from './components/Main'
+import {Helmet} from "react-helmet"
 import "../public/css/style.css"
 
 
 const App = () => {
   return (
     <div className="body">
+        <Helmet>
+            {/* <!-- Primary Meta Tags --> */}
+            <title>Giselle Zatonyl Development Portfolio</title>
+            <meta name="title" content="Giselle Zatonyl Development Portfolio" />
+            <meta name="description" content="" />
+
+            {/* <!-- Open Graph / Facebook --> */}
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="http://dev.gisellezatonyl.com/" />
+            <meta property="og:title" content="Giselle Zatonyl Development Portfolio" />
+            <meta property="og:description" content="" />
+            <meta property="og:image" content="/assets/imgs/link-preview.JPG" />
+
+            {/* <!-- Twitter --> */}
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="http://dev.gisellezatonyl.com/" />
+            <meta property="twitter:title" content="Giselle Zatonyl Development Portfolio" />
+            <meta property="twitter:description" content="" />
+            <meta property="twitter:image" content="/assets/imgs/link-preview.JPG" />
+        </Helmet>
         <Navigation />
         <Header />
         <Intro />
