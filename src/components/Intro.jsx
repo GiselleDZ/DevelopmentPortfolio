bb456cfa3e872c9b57f3a4c883feecd13f47ee71
GiselleDZ <gz@gisellezatonyl.com>
2021-03-25 16:59:49 -0400

    algo expert certificate popup, spacing fixes in popup and cards, card resize to fit more content

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -81,10 +81,19 @@ export default class Intro extends Component {
                                         I'm Giselle, a Front-End-leaning-Full-Stack Software Engineer (who isn't about labels.)  Welcome to my portfolio! 
                                     </p>
                                     <p className="paragraph paragraph--medium-large paragraph--spacious paragraph--thick paragraph--dark u-margin-bottom-small">
-                                        I'm interested in creating memorable user experiences with engaging functionality, captivating design, state-of-the-art tech, and efficient code. 
+                                        I recently pivoted from Graphic Design because it wasn't technical enough. BUT, tbh, once a designer, always a designer..  
                                     </p>
                                     <p className="paragraph paragraph--medium-large paragraph--spacious paragraph--thick paragraph--dark u-margin-bottom-small">
-                                        I recently pivoted from Graphic Design because it wasn't technical enough. BUT, tbh, once a designer, always a designer..  
+                                        I'm interested in creating memorable user experiences with smooth and engaging functionality, captivating design, state-of-the-art tech, and efficient code. 
+                                    </p>
+                                    <p className="paragraph paragraph--medium-large paragraph--spacious paragraph--thick paragraph--dark u-margin-bottom-small">
+                                        I recently acquired&nbsp;
+                                        <a href="#algo-cert" className="paragraph paragraph--link" onClick={(e) => this.noFlip(e)}>
+                                             this algorithms certificate
+                                        </a>&nbsp;from&nbsp;
+                                        <a href="https://algoexpert.io" className="paragraph paragraph--link" rel="noopener noreferrer" target="_blank" onClick={(e) => this.noFlip(e)}>
+                                            Algo Expert
+                                        </a>
                                     </p>
                                 </div>
                                 <div className="card__text">
@@ -92,7 +101,7 @@ export default class Intro extends Component {
                                         As a human, I am mostly composed of Sci-Fi-Noise-Techno Nerd. I love hiking in nature and staying active. I'm also a vocal advocate for BIPOC and LGBTQ+ rights, Net Neutrality, and Climate Justice. I was born in Buenos Aires, Argentina. I'm married to my rad wife Stella, we have two really cute and spoiled cats.
                                     </p>
                                     <p className="paragraph paragraph--medium-large paragraph--spacious paragraph--thick paragraph--dark u-margin-bottom-small">
-                                    Find out more in &nbsp;
+                                    Find out more in&nbsp;
                                         <a href="#biopopup" className="paragraph paragraph--link" onClick={(e) => this.noFlip(e)}>
                                              MY BIO!
                                         </a>
@@ -117,7 +126,12 @@ export default class Intro extends Component {
                 <div className="intro__shape">
                     <img className="intro__shape intro__shape--main" src="https://res.cloudinary.com/delu9m4xu/image/upload/v1599206968/G-Dev/imgs/imgs_composition-02_e0atux.svg"></img>
                 </div>
+
+
+
+                {/* BIO POP-UP */}
                 <section className="popup" id="biopopup">
+                    <a href="#intro" className="popup__background popup__close"></a>
                     <div className="popup__content popup__content--bio">
                         <img src="public\assets\imgs\blue-G-300.jpg" alt="Giselle headshot in blue" className="popup__image--headshot"/>
                         <a href="#intro" className="popup__close">&times;</a>
@@ -128,7 +142,7 @@ export default class Intro extends Component {
                             <p className="paragraph paragraph--light paragraph--big popup__text--bio u-margin-bottom-small">
                                 For the previous 10 years, I worked as Graphic and Motion Designer for companies like CBRE, Gensler and Estee Lauder.</p>
                             <p className="paragraph paragraph--light paragraph--big popup__text--bio u-margin-bottom-small">
-                                Simultaneously, I worked on side projects, focusing on 3D animation, which meant working with software engineers to develop interactive interfaces, for example, a future inspired Art Museum on the web, created in WebGL and ThreeJS. Or&nbsp; <a href="http://www.dream-space.net/artists/zatonyl/" className="popup__link" rel="noopener noreferrer" target="_blank" >this project</a> &nbsp;I built in Unity for Dreamspace. I even built my own &nbsp;<a href="http://www.dream-space.net/artists/zatonyl/" className="popup__link" rel="noopener noreferrer" target="_blank">art portfolio website</a>&nbsp; on Adobe Dreamweaver and Muse. 
+                                Simultaneously, I worked on side projects, focusing on 3D animation, which meant working with software engineers to develop interactive interfaces, for example, a future inspired Art Museum on the web, created in WebGL and ThreeJS. Or&nbsp;<a href="http://www.dream-space.net/artists/zatonyl/" className="popup__link" rel="noopener noreferrer" target="_blank" >this project</a>&nbsp;I built in Unity for Dreamspace. I even built my own &nbsp;<a href="http://www.dream-space.net/artists/zatonyl/" className="popup__link" rel="noopener noreferrer" target="_blank">art portfolio website</a>&nbsp;on Adobe Dreamweaver and Muse. 
                                 Projects like these, drove me to really learn programming, so I joined the Grace Hopper Program to kick start my learning. 
                             </p>
                             <p className="paragraph paragraph--light paragraph--big popup__text--bio u-margin-bottom-small">
@@ -143,6 +157,31 @@ export default class Intro extends Component {
                         </div>
                     </div>
                 </ section>
+
+
+
+                {/* ALGO EXPERT CERTIFICATE POPUP */}
+                <section className="popup" id="algo-cert">
+                    <a href="#intro" className="popup__background popup__close"></a>
+                    <div className="popup__content popup__content--algo">
+                        <a href="#intro" className="popup__close">&times;</a>
+                        <div className="popup__text--bio">
+                            <h2 className="heading-secondary heading-secondary--light u-margin-bottom-smallmedium">
+                                <a href="https://certificate.algoexpert.io/AlgoExpert%20Certificate%20AE-e80857178f" className="heading-secondary heading-secondary--light heading-secondary--link u-margin-bottom-smallmedium" rel="noopener noreferrer" target="_blank" >
+                                    Algorithms Certificate 
+                                </a>&nbsp;from&nbsp;
+                                <a href="https://algoexpert.io" className="heading-secondary heading-secondary--light u-margin-bottom-smallmedium" rel="noopener noreferrer" target="_blank" >
+                                    Algo Expert
+                                </a>
+                            </h2>
+                        </div>
+                        <img src="https://res.cloudinary.com/delu9m4xu/image/upload/c_scale,w_900/v1616704461/G-Dev/imgs/Algo-Expert-Certificate_umwcyg.png" alt="Algo Exp[ert Certificate for the completion of one hundred algorithm interview problems on algoexpert.com" className="popup__content--cert"/>
+                    </div>
+                </ section>
+
+
+
+
             </section>
         )
     }
