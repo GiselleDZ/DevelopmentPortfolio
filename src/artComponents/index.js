c80748b48ebae8af95b184e2842baf8d3145a9df
GiselleDZ <giselle@gisellezatonyl.com>
2021-10-08 20:10:56 -0400

    feat: index third sketch

--- src/artComponents/index.js
+++ src/artComponents/index.js
@@ -1,3 +1,4 @@
 export { default as firstSketch } from "./firstSketch";
 export { default as threeDSketchTest } from "./3dSketchTest";
 export { default as secondSketch } from "./secondSketch";
+export { default as thirdSketch } from "./thirdSketch";
